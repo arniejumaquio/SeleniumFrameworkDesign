0633d12ea318e96feba1e0634ed87be286f5b67f
arniejumaquio <arniejumaquio@gmail.com>
2025-12-24 15:12:06 +0800

    Updated files

--- src/test/java/rahulshettyacademy/testcomponents/BaseTest.java
+++ src/test/java/rahulshettyacademy/testcomponents/BaseTest.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
